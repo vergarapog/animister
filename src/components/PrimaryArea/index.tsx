81427be364ab6ff49497b3376ecd99bed9be31a5
vergarapog <201801166@iacademy.edu.ph>
2023-05-30 16:31:38 +0800

    first iteration for drag mouse func on animation groups

--- src/components/PrimaryArea/index.tsx
+++ src/components/PrimaryArea/index.tsx
@@ -1,8 +1,21 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import React from "react";
 import Animation from "./Animation";
 
+// const {
+//   ScrollMenu,
+//   VisibilityContext,
+//   // eslint-disable-next-line @typescript-eslint/no-var-requires
+// } = require("react-horizontal-scrolling-menu");
+
 import { ScrollMenu, VisibilityContext } from "react-horizontal-scrolling-menu";
 import "react-horizontal-scrolling-menu/dist/styles.css";
+
+import useDrag from "./useDrag";
+
+type scrollVisibilityApiType = React.ContextType<typeof VisibilityContext>;
 
 const getItems = () =>
   Array(20)
@@ -11,10 +24,27 @@ const getItems = () =>
 
 const PrimaryArea = () => {
   const [items, setItems] = React.useState(getItems);
+
+  // NOTE: for drag by mouse
+  const { dragStart, dragStop, dragMove, dragging } = useDrag();
+  const handleDrag =
+    ({ scrollContainer }: scrollVisibilityApiType) =>
+    (ev: React.MouseEvent) =>
+      dragMove(ev, (posDiff) => {
+        if (scrollContainer.current) {
+          scrollContainer.current.scrollLeft += posDiff;
+        }
+      });
+
   return (
     <main className={`h-full py-2`}>
-      <section>
-        <ScrollMenu>
+      <section onMouseLeave={dragStop}>
+        <ScrollMenu
+          onWheel={onWheel}
+          onMouseDown={() => dragStart}
+          onMouseUp={() => dragStop}
+          onMouseMove={handleDrag}
+        >
           <div
             className={`flex space-x-4 overflow-x-scroll p-2 scrollbar-hide`}
           >
@@ -31,5 +61,20 @@ const PrimaryArea = () => {
     </main>
   );
 };
+
+function onWheel(apiObj: scrollVisibilityApiType, ev: React.WheelEvent): void {
+  const isThouchpad = Math.abs(ev.deltaX) !== 0 || Math.abs(ev.deltaY) < 15;
+
+  if (isThouchpad) {
+    ev.stopPropagation();
+    return;
+  }
+
+  if (ev.deltaY < 0) {
+    apiObj.scrollNext();
+  } else if (ev.deltaY > 0) {
+    apiObj.scrollPrev();
+  }
+}
 
 export default PrimaryArea;
