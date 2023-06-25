d4510f6c981ee15ed8eb415a2d21b97aa5349a3b
vergarapog <201801166@iacademy.edu.ph>
2023-06-25 16:10:47 +0800

    add firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA5JhQEbecSMuFyWTvbrFtIZZbZQFeqQOI",
+  authDomain: "animista-clone-ts.firebaseapp.com",
+  projectId: "animista-clone-ts",
+  storageBucket: "animista-clone-ts.appspot.com",
+  messagingSenderId: "999203202190",
+  appId: "1:999203202190:web:78fec2b7c02bbb88de15df",
+  measurementId: "G-JHTQXV862F",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
