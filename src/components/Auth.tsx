0a4373cb045cebb05690e5a7ad908f6e6c2e2a9e
vergarapog <201801166@iacademy.edu.ph>
2023-07-04 15:32:23 +0800

    google auth prototype finished

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,10 +1,59 @@
-import React from "react";
+import { useState } from "react";
+import { auth, googleProvider } from "../config/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signIn = async () => {
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      <input placeholder="Email"></input>
-      <input placeholder="Password" />
+      <input
+        className={`text-black`}
+        placeholder="Email"
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+      ></input>
+      <input
+        className={`text-black`}
+        placeholder="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+      />
+      <button onClick={signIn}>Sign in</button>
+
+      <button onClick={signInWithGoogle}>Sign in With Google</button>
+
+      <button onClick={logout}>Signout</button>
     </div>
   );
 };
