07e3dfc84f83f254aa72df746697bc68b96425f5
vergarapog <201801166@iacademy.edu.ph>
2023-08-18 21:34:32 +0800

    add responsive navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,17 +12,20 @@ const Navbar = () => {
   };
 
   return (
-    <nav className={`px-4 py-2 text-sm uppercase`}>
+    <nav className={`px-2 py-3 text-sm uppercase md:px-4 md:py-2`}>
       {/* <Auth /> */}
-      <div className={`flex flex-col items-center justify-between md:flex-row`}>
+      {/* Desktop Version of Navbar*/}
+      <div
+        className={`hidden flex-col justify-between md:flex md:flex-row md:items-center`}
+      >
         <div className={`flex items-center space-x-5`}>
           <div
-            className={`select-none rounded-full bg-white px-2 font-semibold text-black md:text-3xl`}
+            className={`select-none rounded-full bg-white px-2 text-xl font-semibold text-black md:text-3xl `}
           >
             B
           </div>
-          <div>
-            <ul className="flex space-x-2  font-bold">
+          <div className=" overflow-x-scroll  scrollbar-hide">
+            <ul className="flex space-x-2 ">
               {animationCategories.map((category) => {
                 if (category.title === selectedCategory) {
                   return (
@@ -49,11 +52,57 @@ const Navbar = () => {
             </ul>
           </div>
         </div>
-        <div onClick={() => setIsSideBarOpen(true)}>
+        <div onClick={() => setIsSideBarOpen(true)} className="self-center">
           <FontAwesomeIcon
             className={`cursor-pointer text-xl md:text-2xl`}
             icon="bars"
           />
+        </div>
+      </div>
+      {/* Mobile Version of Navbar*/}
+      <div
+        className={`flex flex-col justify-between space-y-3 md:hidden md:flex-row md:items-center`}
+      >
+        <div className={`flex w-full items-center justify-between`}>
+          <div
+            className={`select-none rounded-full bg-white px-2 text-xl font-semibold text-black md:text-3xl `}
+          >
+            B
+          </div>
+
+          <div onClick={() => setIsSideBarOpen(true)} className="self-center">
+            <FontAwesomeIcon
+              className={`cursor-pointer text-xl md:text-2xl`}
+              icon="bars"
+            />
+          </div>
+        </div>
+        <div className=" overflow-x-scroll  scrollbar-hide">
+          <ul className="flex space-x-2 ">
+            {animationCategories.map((category) => {
+              if (category.title === selectedCategory) {
+                return (
+                  <li
+                    className={`cursor-pointer select-none rounded bg-accent p-1 `}
+                    key={category.id}
+                    onClick={() => handleClick(category.title)}
+                  >
+                    {category.title}
+                  </li>
+                );
+              } else {
+                return (
+                  <li
+                    className={`cursor-pointer select-none p-1 underline-offset-4 transition-all hover:scale-110 `}
+                    key={category.id}
+                    onClick={() => handleClick(category.title)}
+                  >
+                    {category.title}
+                  </li>
+                );
+              }
+            })}
+          </ul>
         </div>
       </div>
     </nav>
